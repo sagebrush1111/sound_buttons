6836ba13b5822cd6a44ff5627b10754106e25457
sagebrush1111 <52252910+sagebrush1111@users.noreply.github.com>
2020-04-05 17:03:23 -0700

    First push of streamlined code-bugs still need to be worked out

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Released under GNU Public License v3
 # Copyright 2020 sagebrush1111
 # Absolutely No WARRANTY Expressed or Implied
-# Prod v2.5
+# Beta v3
 
 from os import environ
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
@@ -12,72 +12,17 @@ import pygame
 
 buf=[]
 
-def play_sound(i):
+def play_sound(b):
     pygame.mixer.init()
-    if i==0:
-        pygame.mixer.music.load("b0.wav")
-        pygame.mixer.music.play()
-    elif i==1:
-        pygame.mixer.music.load("b1.wav")
-        pygame.mixer.music.play()
-    elif i==2:
-        pygame.mixer.music.load("b2.wav")
-        pygame.mixer.music.play()
-    elif i==3:
-        pygame.mixer.music.load("b3.wav")
-        pygame.mixer.music.play()
-    elif i==4:
-        pygame.mixer.music.load("b4.wav")
-        pygame.mixer.music.play()
-    elif i==5:
-        pygame.mixer.music.load("b5.wav")
-        pygame.mixer.music.play()
-    elif i==6:
-        pygame.mixer.music.load("b6.wav")
-        pygame.mixer.music.play()
-    elif i==7:
-        pygame.mixer.music.load("b7.wav")
-        pygame.mixer.music.play()
-    else:
-        print("Not valid pin")
+    pygame.mixer.music.load(sound_map[b])
+    pygame.mixer.music.play()
     while pygame.mixer.music.get_busy() == True:
         continue
     
 def record_button(b):
-    if b.pin.number==18:
-        buf.append(0)
-        print("Pin selected: 0")
-        play_sound(0)
-    elif b.pin.number==23:
-        buf.append(1)
-        print("Pin selected: 1")
-        play_sound(1)
-    elif b.pin.number==24:
-        buf.append(2)
-        print("Pin selected: 2")
-        play_sound(2)
-    elif b.pin.number==25:
-        buf.append(3)
-        print("Pin selected: 3")
-        play_sound(3)
-    elif b.pin.number==12:
-        buf.append(4)
-        print("Pin selected: 4")
-        play_sound(4)
-    elif b.pin.number==16:
-        buf.append(5)
-        print("Pin selected: 5")
-        play_sound(5)
-    elif b.pin.number==20:
-        buf.append(6)
-        print("Pin selected: 6")
-        play_sound(6)
-    elif b.pin.number==21:
-        buf.append(7)
-        print("Pin selected: 7")
-        play_sound(7)
-    else:
-        pass
+    buf.append(buttons[b]);
+    print("Pin selected: ", buttons[b]);
+    play_sound(b);
 
 b0 = Button(18)
 b1 = Button(23)
@@ -88,7 +33,8 @@ b5 = Button(16)
 b6 = Button(20)
 b7 = Button(21)
 bof = Button(4)
-buttons = [b0,b1,b2,b3,b4,b5,b6,b7]
+buttons = {b0:18,b1:23,b2:24,b3:25,b4:12,b5:16,b6:20,b7:21}
+sound_map={b0:"b0.wav",b1:"b1.wav",b2:"b2.wav",b3:"b3.wav",b4:"b4.wav",b5:"b5.wav",b6:"b6.wav",b7:"b7.wav"}
 for b in buttons:
     b.when_pressed=record_button
 print("Select D4 to stop.")    
